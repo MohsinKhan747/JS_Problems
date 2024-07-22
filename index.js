a2abb2faf3e864a5a56c1e721275210cc350cffc
Mohsin <mohsinkhan.0787@gmail.com>
2024-07-23 03:05:48 +0500

    getvowelsCount
    Getting the number of vowels in a string

--- index.js
+++ index.js
@@ -7,7 +7,34 @@ let newString = "MohsinKhan";
 // const newOne = reverseString1.join('');
 // console.log(newOne);
 
-let reverseStr = (str)=>{
-    return str.split("").reverse().join('');
+// let reverseStr = (str)=>{
+//     return str.split("").reverse().join('');
+// }
+// console.log(reverseStr(newString));
+
+
+// return the count of vowels in a current string.
+// a,e,i,o,u
+
+let getCount = (str)=>{
+    let vowelsCount = 0;
+
+    const arr = str.split("");
+    for(let i = 0; i < arr.length; i++){
+        switch(arr[i]){
+            case 'a': vowelsCount++;
+            break;
+            case 'e': vowelsCount++;
+            break;
+            case 'i': vowelsCount++;
+            break;
+            case 'o': vowelsCount++;
+            break;
+            case 'u': vowelsCount++;
+            break;
+        }
+    }
+    return vowelsCount;
 }
-console.log(reverseStr(newString));
+
+console.log(getCount('helloworld'));
